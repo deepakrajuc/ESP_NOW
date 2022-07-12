ed18603e5960a1344dc7b880bb7af4e6aa87e0f5
deepakrajuc <deepakrajuc29@gmail.com>
2022-07-12 20:43:52 +0530

    small change

--- ESP32_NOW/src/main.cpp
+++ ESP32_NOW/src/main.cpp
@@ -6,6 +6,7 @@ void setup ()
   WiFi.mode(WIFI_MODE_STA);//make the esp32 as the station mode
   Serial.print("MAC Address;");//print the esp32 mac address in the serial monitor
   Serial.print(WiFi.macAddress());
+  Serial.print("mac address in updated");
 }
 void loop()
 {
