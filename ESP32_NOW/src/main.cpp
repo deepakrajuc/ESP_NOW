688cc8df99819b7ed9dbfc5c0016b5ece8791c4f
deepakrajuc <deepakrajuc29@gmail.com>
2022-07-12 19:07:05 +0530

    getting MAC address of esp

---
+++ ESP32_NOW/src/main.cpp
@@ -0,0 +1,13 @@
+#include "WiFi.h"
+
+void setup ()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);//make the esp32 as the station mode
+  Serial.print("MAC Address;");//print the esp32 mac address in the serial monitor
+  Serial.print(WiFi.macAddress());
+}
+void loop()
+{
+  
+}
